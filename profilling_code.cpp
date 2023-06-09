d07a87a3a8bfebb5c07e56cad3dc739d98318699
Rajpreet2206 <rajpreet2206@gmail.com>
2023-06-09 16:03:27 +0200

    A basic cpp code to profile functions....in terms of milliseconds taken by a certain function...It's like "passing a function as an argument to another function"...chorno function taking other functions as arguments

--- profilling_code.cpp
+++ profilling_code.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+#include<cmath>
+#include<chrono>
 
+double variable1;
+double variable2;
+
+void initialize(){
+    
+    for(int i=0; i< 1000000000; ++i){
+        variable1 = 2 + i;
+    }
+}
+
+void another_init(){
+    for(int j=0; j<1000000000; ++j){
+        variable2 = 2*j;
+    }
+}
+
+float time_taken_by_the_function(void(*function)()){
+    auto Tstart = std::chrono::high_resolution_clock::now();
+    function();
+    auto Tend = std::chrono::high_resolution_clock::now();
+    auto time_duration = std::chrono::duration_cast<std::chrono::milliseconds>(Tend - Tstart).count();
+    std::cout<<"Executing the function took "<< time_duration << "milliseconds" << std::endl;
+    return time_duration;
+}
+
+int main(){
+    std::cout<<"Hi...Check for Profiling...Results coming through"<< std::endl;
+    initialize();
+    std::cout<<"The initialized value of the variable1 is " << variable1 << std::endl;
+    auto time_taken = time_taken_by_the_function(&initialize);
+    std::cout<<"Time taken, returned as a RETURN value of the function initalize is " << time_taken << std::endl;
+    
+        
+    another_init();
+    std::cout<<"The initialized value of the variable2 is " << variable2 << std::endl;
+    auto time_taken2 = time_taken_by_the_function(&another_init);
+    std::cout<<"Time taken, returned as a RETURN value of the function another_init is " << time_taken2 << std::endl;
+    return 0;
+}
